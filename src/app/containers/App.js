14f5c9007ba5016b60708c94dce57580bc7edfaf
skratchdot <git@skratchdot.com>
2015-12-14 18:32:46 -0500

    using react-github-corner

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
 import pathGet from 'object-path-get';
+import GithubCorner from 'react-github-corner';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import GithubCorner from '../components/GithubCorner';
 import stringToCssName from '../helpers/stringToCssName';
 
 class App extends Component {
